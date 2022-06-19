29d430e86f1c763a33afb8d4a4834fd91d3b1380
Andrei Shapel <mail@shapel.me>
2022-06-19 18:55:57 +0200

    Add logic to 'crypto' WASM component

--- server/src/services/crypto/main.rs
+++ server/src/services/crypto/main.rs
@@ -1,25 +1,29 @@
 use marine_rs_sdk::marine;
 use marine_rs_sdk::module_manifest;
 use base64::{encode};
-use md5::{Md5, Digest};
+use md5::{Md5};
+use sha2::{Sha256, Digest};
 
 module_manifest!();
 
 pub fn main() {}
 
 fn hash_text_with_algorithm_selected(algorithm: &str, text: &str) -> String {
-  let mut result = Default::default();
-
   match algorithm {
     "md5" => {
       let mut hasher = Md5::new();
       hasher.update(text);
-      result = hasher.finalize();
+      let result = hasher.finalize();
+      encode(result)
     },
-    _ => println!("Abc")
+    "sha256" => {
+      let mut hasher = Sha256::new();
+      hasher.update(text);
+      let result = hasher.finalize();
+      encode(result)
+    },
+    _ => panic!("No algorithm selected"),
   }
-
-  return encode(result);
 }
 
 #[marine]
