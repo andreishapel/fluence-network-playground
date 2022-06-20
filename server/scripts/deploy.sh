8f654f8e6ceba6c27e7a41f2b845052ad4c7ad60
Andrei Shapel <mail@shapel.me>
2022-06-21 00:37:37 +0200

    Add 'Config.toml'

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
+SERVICE=$1
+SECRET_KEY=${2-Pfw9nfmCAVVONZAIZAnzyWMNTnKgn+3u3J/p7xSVxw8=}
+ADDRESS=${3-/dns4/kras-04.fluence.dev/tcp/19001/wss/p2p/12D3KooWFEwNWcHqi9rtsmDhsYcDbRUCDXH84RC4FW6UfsFWaoHi}
+
 aqua remote deploy_service \
-  --addr /dns4/kras-04.fluence.dev/tcp/19001/wss/p2p/12D3KooWFEwNWcHqi9rtsmDhsYcDbRUCDXH84RC4FW6UfsFWaoHi \
-  --sk Pfw9nfmCAVVONZAIZAnzyWMNTnKgn+3u3J/p7xSVxw8= \
+  --addr $ADDRESS \
+  --sk $SECRET_KEY \
   --config-path configs/deployment.json \
-  --service crypto
+  --service $SERVICE
