abb79439f0c850e5b78f72925d35517a530cf934
Andrei Shapel <mail@shapel.me>
2022-06-23 16:04:41 +0200

    Add curl service

--- server/src/services/price/main.rs
+++ server/src/services/price/main.rs
@@ -1,26 +1,3 @@
-// use marine_rs_sdk::marine;
-// use marine_rs_sdk::module_manifest;
-//
-// module_manifest!();
-//
-// pub fn main() {}
-//
-// #[marine]
-// pub struct Response {
-//   crypto_currency: String,
-//   fiat_currency: String,
-//   price: String,
-// }
-//
-// #[marine]
-// pub fn hash_text(algorithm: &str, text: &str) -> Response {
-//   Response {
-//     crypto_currency: format!("{}", 1),
-//     fiat_currency: format!("{}", 2),
-//     price: format!("{}", 3),
-//   }
-// }
-
 #![allow(improper_ctypes)]
 
 use marine_rs_sdk::marine;
@@ -33,11 +10,9 @@ pub fn main() {}
 
 #[marine]
 pub fn download(url: String) -> String {
-  let result = unsafe {
-    curl(vec![url])
-  };
-
-  String::from_utf8(result.stdout).unwrap()
+  let result = unsafe { curl(vec![url]) };
+  let output = result.stdout;
+  String::from_utf8(output).unwrap()
 }
 
 #[marine]
